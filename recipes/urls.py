08aefa6dbded9b53917ad8776d90f49d402ae0f8
Henrique Guilherme <gxhenriquecma2022@gmail.com>
2025-11-22 09:17:53 -0300

    alteração

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,8 +1,21 @@
 from django.urls import path
-from recipes.views import home
 
+# from recipes.views import home
+
+from recipes.views import home
+from recipes.views import recipe
+from recipes.views import category
+
+#criar "name" para as url para que elas fiquem dinamicas
+
+app_name = 'recipes'
 
 urlpatterns = [
-    path('', home),
+    path('', home, name= 'recipes-home'),
+    path('recipes/category/<int:category_id>/', category, name= 'category'),
+    path('recipes/<int:id>/', recipe, name= 'recipes-recipe')
    
 ]
+
+#  path('recipes/category/<int:category_id>/',
+#          views.category, name="category"),
--- recipes/views.py
+++ recipes/views.py
@@ -1,9 +1,32 @@
 from django.shortcuts import render
+from utils.recipes.factory import make_recipe
+
+from recipes.models import Recipe
 
 # Create your views here.
 
+# esse for serve para gerar 10 ou mais conteudo dinamicamente
+# mas tem que criar o stript de automação
 
 def home(request):
-    return render(request, 'recipes/pages/home.html')
+    recipes = Recipe.objects.filter(is_published=True).order_by('-id')
+    return render(request, 'recipes/pages/home.html', context={
+        'recipes': recipes,
+    })
+
+def category(request,category_id):
+    recipes = Recipe.objects.filter(category__id=category_id, is_published=True).order_by('-id')
+    return render(request, 'recipes/pages/home.html', context={
+        'recipes': recipes,
+    })
+
+def recipe(request,id):
+    recipes = Recipe.objects.all().order_by('-id')
+    return render(request, 'recipes/pages/recipe-view.html', context={
+        'recipe': make_recipe(),
+        'is_detail_page': True,
+        
+    })
+
 
    
